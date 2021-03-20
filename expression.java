5ee659ce1e68e7da81eac354c87b96803ca73f2f
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-03-20 14:20:46 +0900

    課題1

---
+++ expression.java
@@ -0,0 +1,27 @@
+package hellojava;
+
+public class expression {
+
+	public static void main(String[] args) {
+		int age;
+		age=100;
+		System.out.println(age);
+
+		double weight;
+		weight = 100.12;
+		System.out.println(weight);
+
+		System.out.println("100");
+
+		boolean isError;
+		isError = true;
+        System.out.println(isError);
+
+        boolean result;
+		result = false;
+        System.out.println(result);
+
+
+	}
+
+}
