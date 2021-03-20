d5162977536ffef8763f2eaed4a4edb3ad2543de
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-03-20 15:41:43 +0900

    課題3

---
+++ expression3.java
@@ -0,0 +1,12 @@
+package hellojava;
+
+public class expression3 {
+
+	public static void main(String[] args) {
+		double pi = 3.1415;
+		int diameter = 100;
+		System.out.println(diameter*pi);
+
+	}
+
+}
