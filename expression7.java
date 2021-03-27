93ebb6f67fcf39ef6f20c6c9fbc6f4841ccf28ca
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-03-27 15:17:12 +0900

    課題7

---
+++ expression7.java
@@ -0,0 +1,19 @@
+package hellojava;
+
+public class expression7 {
+
+	public static void main(String[] args) {
+
+		int result = 1;
+
+        for (int i = 0; i < 29; i++) {
+
+        result = result * 2;
+
+        System.out.println(result);
+
+        }
+
+	}
+
+}
