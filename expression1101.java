289cbd2258186b0532f5025566bf522b68daf69a
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-03-29 16:44:39 +0900

    課題11-01

---
+++ expression1101.java
@@ -0,0 +1,31 @@
+package hellojava2;
+
+public class expression1101 {
+
+	public static void main(String[] args) {
+		
+		int age;
+		age = 100;
+		System.out.println(age);
+		
+		float number;
+		number = 100.12f;
+		System.out.println(number);
+
+		double myNumber;
+		myNumber = 100.12;
+		System.out.println(myNumber);
+
+		System.out.println("100");
+
+		boolean a;
+		a = true;
+		System.out.println(a);
+		
+		boolean b;
+		b = false;
+		System.out.println(b);
+
+	}
+
+}
