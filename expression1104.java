a47d33f3786bc7d433a22256853fc31fe7146265
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-03-29 16:56:43 +0900

    課題11-04

---
+++ expression1104.java
@@ -0,0 +1,12 @@
+package hellojava2;
+
+import java.math.BigDecimal;
+
+public class expression1104 {
+
+	public static void main(String[] args) {
+		
+		BigDecimal a = new BigDecimal("0.1");
+		System.out.println(a.add(a).add(a));
+	}
+}
