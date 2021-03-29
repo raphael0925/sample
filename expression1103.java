d8a37190500ab8a2eb6e52a2f6047d2030024557
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-03-29 16:54:17 +0900

    課題11-03

---
+++ expression1103.java
@@ -0,0 +1,13 @@
+package hellojava2;
+
+public class expression1103 {
+
+	public static void main(String[] args) {
+		
+		int dia = 100;
+		double pi = 3.1415;
+//		double result = dia * pi; 10行目にresultを代入したい。
+		System.out.println("直径"+dia+"の円周は"+(dia*pi)+"です。");
+	}
+
+}
