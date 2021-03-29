7f26d20a180ea774abe6992e63932ee1d7759dae
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-03-29 16:51:13 +0900

    課題11-02

---
+++ expression1102.java
@@ -0,0 +1,15 @@
+package hellojava2;
+
+public class expression1102 {
+
+	public static void main(String[] args) {
+
+		int number1;
+		number1 = 100;
+		
+		int number2;
+		number2 = 3;
+		
+		System.out.println(number1*number2);
+	}
+}
