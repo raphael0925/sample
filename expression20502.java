8006386b4646c7e7ad671b44a5ace6322aaaf466
raphael0925 <80556321+raphael0925@users.noreply.github.com>
2021-04-24 15:50:05 +0900

    課題　配列

--- expression20502.java
+++ expression20502.java
@@ -4,42 +4,11 @@ public class expression20502 {
 
 	public static void main(String[] args) {
 		String str =
-				"The,
-	    		scientific,
-	    		name,
-	            persica,
-	    		along,
-	    		with,
-	    		the,
-	    		word,
-	    		peach,
-	    		itself,
-	    		and,
-	    		its,
-	    		cognates,
-	    		in,
-	    		many,
-	    		European,
-	    		languages,
-	    		derives,
-	    		from,
-	    		an,
-	    		early,
-	    		European,
-	    		belie,
-	    		that,
-	    		peaches,
-	    		were,
-	    		native,
-	    		to,
-	    		Persia,
-	    		(modern-day,
-	    		Iran).";
-
-		String[] words = str.split(",");
+			"The scientific name persica, along with the word \"peach\" itself and its cognates in many European languages, derives from an early European belief that peaches were native to Persia (modern-day Iran).";
+		String[] words = str.split(" ");
 
 	    for (int i = 0; i < words.length; i++) {
-	      System.out.println(words[i]);
+	    	System.out.println(words[i]);
 	    }
 
 	}
